67e25dc2b5260c47e544480c9f9521b8ecb1a94f
Vivek Suryawanshi <coderviky@gmail.com>
2021-07-08 14:45:15 +0530

    Repairit Backend - Dockerise Django Postgresql project created

---
+++ app/repairit/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for repairit project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repairit.settings')
+
+application = get_asgi_application()
