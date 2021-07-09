89b7f6743027acd1f5f763c5466e8432cdee80c5
Karishma Nikam <karishmanikam282@gmail.com>
2021-07-09 18:35:18 +0530

    4 apps customer repairman repairjob review created

--- app/repairit/settings.py
+++ app/repairit/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customer',
+    'repairjob',
+    'repairman',
+    'review',
 ]
 
 MIDDLEWARE = [
