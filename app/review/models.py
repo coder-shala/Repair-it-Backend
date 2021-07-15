13ee6ea294d4adc465b70c989fa9dba003ca29ca
Karishma Nikam <karishmanikam282@gmail.com>
2021-07-15 14:13:00 +0530

    models added

--- app/review/models.py
+++ app/review/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Repairjob(models.Model):
+    pass
+class Repairman(models.Model):
+    pass
+class Customer(models.Model):
+    pass
 class Review(models.Model):
     pass
