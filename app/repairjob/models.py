c679a8c1a35c12ff98513cd151575f4e70c2b2ea
Karishma Nikam <karishmanikam282@gmail.com>
2021-07-22 13:36:51 +0530

    fields added

--- app/repairjob/models.py
+++ app/repairjob/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Repairjob(models.Model):
+    RJobID = models.CharField(max_length=50)
+    CustomerID=models.IntegerField()
+    ProductName=models.CharField(max_length=50)
+    ProducDescription=models.CharField(max_length=50)
+    CreatedDate=models.DateField()
+    statusForCustomer=models.CharField
