7bfad9a5dc0358b33394d40073eda1873e373e33
Karishma Nikam <karishmanikam282@gmail.com>
2021-07-24 13:02:22 +0530

    check added models

--- app/customer/models.py
+++ app/customer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    CustomerID=models.AutoField(primary_key=True)
+    CustomerName=models.CharField(max_length=50)
+    MobileNo=models.IntegerField()
+    City=models.CharField(max_length=30)
--- app/repairman/models.py
+++ app/repairman/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 # Create your models here.
+class Repairman(models.Model):
+    repairmanID=models.AutoField(primary_key=True)  #pk
+    repaiemanName=models.CharField(max_length=50)
+    MobileNo=models.IntegerField()
+    City=models.CharField(max_length=40)
+    Address=models.TextField()
+    Skills=models.JSONField()
+    DateJoin=models.DateField()
+class RepairmanSkill(models.Model):
+    RepairManSkillID=models.AutoField(primary_key=True)
+    SkillText=models.CharField(max_length=50)
